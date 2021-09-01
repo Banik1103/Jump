a30ea0481354bfbe17b890561cd6e865fdf10821
SnowballSH <66022611+SnowballSH@users.noreply.github.com>
2021-08-31 20:52:53 -0500

    switch to interpreted

--- main.py
+++
@@ -1,9 +0,0 @@
-from codegen.model import CodeGen
-
-if __name__ == "__main__":
-    model = CodeGen()
-    model.add_loop(["int a = 5;", "a++;", "cout << a << \"\\n\\n\";"])
-    model.add_loop(["auto b = \"Hello, world!\\n\";", "cout<<b<<b<<b<<'\\n';"])
-    model.add_loop([])
-    model.finish()
-    print(model.result)
